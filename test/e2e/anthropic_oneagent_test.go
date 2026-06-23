516aa09fdb4340a0f447b3830af597a77c60e37b
Florian Bacher <florian.bacher@dynatrace.com>
2026-06-23 13:06:38 +0200

    fix wrong dql filter

--- test/e2e/anthropic_oneagent_test.go
+++ test/e2e/anthropic_oneagent_test.go
@@ -11,7 +11,7 @@ func TestAnthropicOneAgent(t *testing.T) {
 	auditSpan(t, "anthropic", "oneagent", GenericProfile,
 		`fetch spans, from: now()-10m
 | filter service.name == "anthropic/oneagent"
-| filter gen_ai.provider.name == "aws_bedrock" and dt.openpipeline.source == "oneagent"
+| filter gen_ai.provider.name == "anthropic" and dt.openpipeline.source == "oneagent"
 | filter isNotNull(gen_ai.request.model)
 | sort timestamp desc
 | limit 1`)
