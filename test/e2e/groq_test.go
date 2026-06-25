11b43aeb8d35ec03cddfc785e4260e363d6eb85b
RealAnna <anna.reale@dynatrace.com>
2026-06-25 10:45:11 +0200

    feat: add proxy

--- test/e2e/groq_test.go
+++ test/e2e/groq_test.go
@@ -9,11 +9,10 @@ func TestGroqOneAgent(t *testing.T) {
 	startApp(t, "groq/oneagent")
 	triggerHaiku(t, false)
 
-	// TODO: confirm gen_ai.system / gen_ai.provider.name emitted by OneAgent
-	// for the Groq SDK and tighten the provider filter once span metadata is known.
 	auditSpan(t, "groq", "oneagent", GenericProfile,
 		`fetch spans, from: now()-10m
 | filter service.name == "groq/oneagent"
+| filter gen_ai.provider.name == "groq"
 | filter dt.openpipeline.source == "oneagent"
 | filter isNotNull(gen_ai.request.model)
 | sort timestamp desc
